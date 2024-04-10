ab8da7ef165868bd700575836adc5de6c977cc8b
israelwalden <waldenisraelk@gmail.com>
2024-04-10 14:39:09 +0000

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,7 +10,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	/*FREE NODE MEMORY*/
 	/*If node is no NULL, free memory*/
-	if(tree != NULL)
+	if (tree != NULL)
 	{
 		binary_tree_delete(tree->left);/*Free left memeber node*/
 		binary_tree_delete(tree->right);/*Free right member node*/
